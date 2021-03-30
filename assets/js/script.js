7eae82cf8d01f855a84e8436623e15138db14556
ceshort23 <ceshort23@gmail.com>
2021-03-30 12:07:38 -0500

    submit user content from form

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,24 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
     event.preventDefault();
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // CREATE LIST ITEM
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // CREATE <div> TO HOLD TASK INFO AND ADD TO LIST ITEM
+    var taskInfoEl = document.createElement("div");
+        // GIVE IT A CLASS NAME
+    taskInfoEl.className = "task-info";         
+        // ADD HTML CONTENT TO <div>
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // ADD ENTIRE LIST ITEM TO LIST
     tasksToDoEl.appendChild(listItemEl);
 }
 
